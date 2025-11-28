3d76fc0f47b58b259004d9823da1d755863b4732
Adefarati22 <adefarati22@gmail.com>
2025-11-28 17:31:15 +0100

    added new effect

--- client/src/component/Card.jsx
+++ client/src/component/Card.jsx
@@ -12,8 +12,19 @@ export default function ProjectCard({
   Role,
 }) {
   return (
-    <motion.div whileHover={{ scale: 1.03 }} transition={{ duration: 0.3 }} className="flex flex-col lg:flex-row items-center justify-between gap-10">
-      <div className="bg-[#1a1a1a] rounded-lg w-full md:h-full lg:h-[70vh]"><img src={src} alt={alt} className="mx-auto block w-full h-full object-cover rounded-lg p-10" /></div>
+    <motion.div
+      whileHover={{ scale: 1.03 }}
+      transition={{ duration: 0.3 }}
+      className="flex flex-col lg:flex-row items-center justify-between gap-10"
+    >
+      <div className="bg-[#1a1a1a] rounded-lg w-full md:h-full lg:h-[70vh]">
+        <img
+          src={src}
+          alt={alt}
+          loading="lazy"
+          className="mx-auto block w-full lg:h h-full object-cover rounded-lg p-10"
+        />
+      </div>
 
       <div className="p-5 w-full ">
         <h3 className="text-xl md:text-4xl font-medium mb-2">{title}</h3>
@@ -39,7 +50,7 @@ export default function ProjectCard({
             rel="noopener noreferrer"
             className="text-[#d3e97a] font-medium border-b-2 flex gap-1 justify-center items-center text-xs md:text-sm"
           >
-            Live Demo <RiArrowRightUpLine size={26}/>
+            Live Demo <RiArrowRightUpLine size={26} />
           </a>
           <a
             href={githubUrl}
@@ -47,7 +58,7 @@ export default function ProjectCard({
             rel="noopener noreferrer"
             className="text-[#d3e97a] font-medium border-b-2 flex gap-1 justify-center items-center text-xs md:text-sm"
           >
-            See on GitHub <img src="/git.svg" alt="github"/>
+            See on GitHub <img src="/git.svg" alt="github" />
           </a>
         </div>
       </div>
